2df4e6fb2219d02a0ba93799a1d39b6ab4abec7e
Sanjjushri <sanjjushrivarshini@gmail.com>
2025-05-05 19:38:34 +0530

    server authtentication added

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import ssl
 
 app = Flask(__name__)
 
@@ -7,5 +8,9 @@ def index():
     return "Hello from Flask with mTLS!"
 
 if __name__ == "__main__":
-    context = ('server.crt', 'server.key')  # Server's certificate and key
+    context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    context.load_cert_chain(certfile="server.crt", keyfile="server.key")
+    context.load_verify_locations(cafile="ca.crt")  # Trust this CA
+    context.verify_mode = ssl.CERT_REQUIRED  # Require client certs
+
     app.run(host='0.0.0.0', port=5001, ssl_context=context)
